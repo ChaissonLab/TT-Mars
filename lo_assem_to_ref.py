5a697e2939250c0602de4e904702107addc6f263
quentin0515 <jianzhiy@usc.edu>
2021-02-03 16:49:07 -0800

    delete dir info

--- lo_assem_to_ref.py
+++ lo_assem_to_ref.py
@@ -1,15 +1,3 @@
-
-# %%
-#In this file: Liftover hg38 reference to HG002 assembly contigs using samLiftover
-#liftover results in ../../data_files/hg19/lo_pos_assem1_withSed_result.bed and ../../data_files/hg19/lo_pos_assem2_withSed_result.bed
-
-# %%
-#minimap2 results: no secondary alignments
-
-#minimap2 -x asm20 -ac --secondary=no /panfs/qcb-panasas/jingwenr/reference/CLR/human_hs37d5.fasta /home/cmb-16/nobackups/mjc/data_download/HG002/CCS/assembly.1.consensus.fasta > ../../data_files/hg19/HG002_to_hg37_asm20_woSed_assem1.sam
-#minimap2 -x asm20 -ac --secondary=no /panfs/qcb-panasas/jingwenr/reference/CLR/human_hs37d5.fasta /home/cmb-16/nobackups/mjc/data_download/HG002/CCS/assembly.2.consensus.fasta > ../../data_files/hg19/HG002_to_hg37_asm20_woSed_assem2.sam
-
-
 # %%
 #generate bed file that contains positions on ref to be lifted to contigs
 import csv
@@ -38,11 +26,9 @@ def isKthBitSet(n, k):
 #for assem1
 #generate bed file liftover
 
-#TODO: change output dir
-#g = open("../../data_files/hg19/lo_pos_assem1_withSed.bed", "w")
+#TODO: change output 
 g = open(output_dir + "lo_pos_assem1_withSed.bed", "w")
 
-#samfile = pysam.AlignmentFile("../../data_files/hg19//HG002_to_hg37_asm20_woSed_assem1.bam", "rb")
 samfile = pysam.AlignmentFile(assembly_bam_file_hap1, "rb")
 for record in samfile:
     #TODO: solve the unmapped segment problem???
@@ -70,10 +56,8 @@ for record in samfile:
 g.close()
 
 #for assem2
-#g = open("../../data_files/hg19/lo_pos_assem2_withSed.bed", "w")
 g = open(output_dir + "lo_pos_assem2_withSed.bed", "w")
 
-#samfile = pysam.AlignmentFile("../../data_files/hg19/HG002_to_hg37_asm20_woSed_assem2.bam", "rb")
 samfile = pysam.AlignmentFile(assembly_bam_file_hap2, "rb")
 for record in samfile:
     #TODO: solve the unmapped segment problem???
@@ -99,26 +83,3 @@ for record in samfile:
             g.write("\n")
 
 g.close()
-
-# %%
-#run samLiftover
-
-#/home/cmb-16/mjc/quentin/illuVeri/use_mcutils/mcutils/bin/samLiftover ../../data_files/hg19/HG002_to_hg37_asm20_woSed_assem1.sam ../../data_files/hg19/lo_pos_assem1_withSed.bed ../../data_files/hg19/lo_pos_assem1_withSed_result.bed --dir 1
-#mapdb 17971
-#example results: chr1/contig_1   382     383     chr1/contig_1   1       51184500        51184501
-
-#/home/cmb-16/mjc/quentin/illuVeri/use_mcutils/mcutils/bin/samLiftover ../../data_files/hg19/HG002_to_hg37_asm20_woSed_assem2.sam ../../data_files/hg19/lo_pos_assem2_withSed.bed ../../data_files/hg19/lo_pos_assem2_withSed_result.bed --dir 1
-#mapdb 18803
-
-
-
-
-# %%
-#get file to double check in IGV
-
-#whole alignment bam file
-#minimap2 -x asm20 -ac --secondary=no /panfs/qcb-panasas/jingwenr/reference/CLR/human_hs37d5.fasta /home/cmb-16/nobackups/mjc/data_download/HG002/CCS/assembly.1.consensus.fasta | samtools sort -o HG002_to_hg37_asm20_woSed_assem1.bam
-#samtools index -@4 HG002_to_hg37_asm20_woSed_assem1.bam
-
-#minimap2 -x asm20 -ac --secondary=no /panfs/qcb-panasas/jingwenr/reference/CLR/human_hs37d5.fasta /home/cmb-16/nobackups/mjc/data_download/HG002/CCS/assembly.2.consensus.fasta | samtools sort -o HG002_to_hg37_asm20_woSed_assem2.bam
-#samtools index -@4 HG002_to_hg37_asm20_woSed_assem2.bam
