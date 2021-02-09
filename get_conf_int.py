6b75b3aa217b97729a2647f9632da9e847554d18
quentin0515 <jianzhiy@usc.edu>
2021-02-09 11:01:47 -0800

    delete short reads fil

--- get_conf_int.py
+++ get_conf_int.py
@@ -6,94 +6,27 @@ import sys
   
 #get command line input
 #n = len(sys.argv)
-output_dir = sys.argv[1]
+output_dir = sys.argv[1] + "/"
 bam_file1 = sys.argv[2]
 bam_file2 = sys.argv[3]
+if_hg38_str = sys.argv[4]
 
-k = 250000
+if if_hg38_str == "True":
+    if_hg38 = True
+else:
+    if_hg38 = False
+#if_hg38 = True
 
-#bam_file = "/scratch2/jingwenr/assembly/HG00514/HG00514_CCS_SS_PG_PRR/h1.bam"
-#bam_file1 = "../../data_files/hg19//HG002_to_hg37_asm20_woSed_assem1.bam"
-#bam_file2 = "../../data_files/hg19//HG002_to_hg37_asm20_woSed_assem2.bam"
+avg_read_depth = sys.argv[5]
 
-if_hg38 = True
-#output_dir = "../../output/v4.0/HG00514_CCS_SS_PG_PRR/"
-#output_dir = "../../output/v4.0/naive_caller_0810_v1/"
 
 chr_list = ["1", "2", "3", "4", "5",
             "6", "7", "8", "9", "10",
             "11", "12", "13", "14", "15",
             "16", "17", "18", "19", "20",
             "21", "22", "X", "Y"]
-
-#chr_list = ["1"]
-
-samfile = pysam.AlignmentFile(bam_file1, "rb")
-g = open(output_dir + "assem1_short_reads_regions_" + str(k) + ".bed", "w")
-for chr_name in chr_list:
-    #test
-    #print(chr_name)
-    #stop when parsed no_of_reads
-    #no_of_reads = 400000
-    counter_read = 0
-    
-    #loop through iter, index will not be reset
-    if if_hg38:
-        ref_name = "chr" + chr_name
-    else:
-        ref_name = chr_name
-        
-    iter = samfile.fetch(ref_name)
-    
-    for rec in iter:
-        #print(rec.infer_query_length())
-        if (rec.infer_query_length() < k):
-            g.write(str(ref_name) + "\t")
-            g.write(str(rec.reference_start) + "\t")
-            g.write(str(rec.reference_end))
-            g.write("\n")
-g.close()
-
-samfile = pysam.AlignmentFile(bam_file2, "rb")
-g = open(output_dir + "assem2_short_reads_regions_" + str(k) + ".bed", "w")
-for chr_name in chr_list:
-    #test
-    #print(chr_name)
-    #stop when parsed no_of_reads
-    #no_of_reads = 400000
-    counter_read = 0
-    
-    #loop through iter, index will not be reset
-    if if_hg38:
-        ref_name = "chr" + chr_name
-    else:
-        ref_name = chr_name
-        
-    iter = samfile.fetch(ref_name)
-    
-    for rec in iter:
-        #print(rec.infer_query_length())
-        if (rec.infer_query_length() < k):
-            g.write(str(ref_name) + "\t")
-            g.write(str(rec.reference_start) + "\t")
-            g.write(str(rec.reference_end))
-            g.write("\n")
-g.close()
-
-
-######################################################################################
-######################################################################################
 
 #Get regions on ref where its not covered by at least one of the assembly
-
-#output_dir = "../../output/v4.0/HG00514_CCS_SS_PG_PRR/"
-#output_dir = "../../output/v4.0/naive_caller_0810_v1/"
-
-chr_list = ["1", "2", "3", "4", "5",
-            "6", "7", "8", "9", "10",
-            "11", "12", "13", "14", "15",
-            "16", "17", "18", "19", "20",
-            "21", "22", "X", "Y"]
 
 #chr_list = ["1"]
 
@@ -154,3 +87,9 @@ for chr_name in chr_list:
                 cur_end = rec.reference_end
 
 g.close()
+
+##################################################################
+##################################################################
+
+#Get regions where read depth > 2 * avg_read_depth
+
