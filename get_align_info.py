7c2efb9e661c04a144a6ce3c799f0efd5140ca99
quentin0515 <jianzhiy@usc.edu>
2021-02-17 23:55:07 -0800

    modify get align info

--- get_align_info.py
+++ get_align_info.py
@@ -165,3 +165,281 @@ with open(liftover_file) as f:
 f.close()
 
 
+
+# %%
+#get vcf file and run score_callset on each SV record
+
+f = pysam.VariantFile(vcf_file,'r')
+
+query_file = query_file1
+hap = 1
+name_str = "assem1"
+chromosome = "all"
+output_file_name = output_dir + "align_info_" + name_str + "_chr" + chromosome + ".txt"
+
+query_fasta_file = pysam.FastaFile(query_file)
+ref_fasta_file = pysam.FastaFile(ref_file)
+ref_name = ""
+
+
+g = open(output_file_name, "w")
+for counter, rec in enumerate(f.fetch()):
+    name = rec.chrom
+
+    #choose target chr
+    #if str(name) != chromosome:
+    #    continue
+
+    sv_type = rec.info['SVTYPE']
+    
+    if sv_type != 'DEL' and sv_type != 'INS':
+        print("Wrong type!")
+        continue
+        
+    #sv_len = abs(rec.info["SVLEN"][0])
+    #without abs value: rela length closer to 1 the better
+    sv_len = rec.info['SVLEN'][0]
+    
+    sv_pos = rec.pos
+    sv_end = rec.stop
+    sv_ref_seq = rec.ref
+    #TODO: update here when multi-calls in one records allowed
+    sv_query_seq = rec.alts[0]
+
+    #find given fragment (on ref) to queries
+    
+    #get ref seq
+    if ref_name != name:
+        ref_name = name
+        ref_rec = ref_fasta_file.fetch(ref_name)
+
+    ref_start = getRefStart(sv_pos)
+    ref_end = getRefEnd(sv_end)
+
+    #first level key: chr index as an int
+    int_ref_name = get_int_chr_name(ref_name)
+    lo_list_index = int_ref_name - 1
+    first_key = lo_list_index
+    
+    #second level key: ref list_pos
+
+    if counter % 1000 == 0:
+        print(counter)
+    
+    #Search for the best second_key_start and second_key_end in the regions
+    region_len_m = 500
+    min_diff_len = 100000000
+    ref_start_best = ref_start
+    ref_end_best = ref_end
+    query_start_best = None
+    query_end_best = None
+    start_contig_name_ctr = -1
+    end_contig_name_ctr = -1
+        
+    neg_strand = False
+    
+    for ref_end_cand in range(ref_end, ref_end+region_len_m, interval):
+        for ref_start_cand in range(ref_start, ref_start-region_len_m, -interval):
+            
+            #second_key_start = str(ref_start_cand)
+            #second_key_end = str(ref_end_cand)
+            second_key_start = ref_start_cand//interval
+            second_key_end = ref_end_cand//interval
+            
+            start_contig_name_ctr_cand = contig_name_list[first_key][second_key_start]
+            end_contig_name_ctr = contig_name_list[first_key][second_key_end]
+            
+            if start_contig_name_ctr_cand == -1 or end_contig_name_ctr == -1:
+                #print("wrong second key")
+                message = "wrong_sec_key"
+                #write_err(output_file_name, message, g)
+                continue
+
+            if start_contig_name_ctr_cand != end_contig_name_ctr:
+                #print("Not same contig")
+                message = "not_same_contig"
+                #write_err(output_file_name, message, g)
+                continue
+                
+            query_start = contig_pos_list[first_key][second_key_start]
+            query_end = contig_pos_list[first_key][second_key_end]
+            
+            neg_strand_tep = False
+            #in case: negtive strand
+            if query_end < query_start:
+                tem = query_start
+                query_start = query_end
+                query_end = tem
+                neg_strand_tep = True
+            
+            #take the best relative length to be the optimal interval
+            if abs((query_end - query_start) - (ref_end_cand - ref_start_cand) - sv_len) < min_diff_len:
+                min_diff_len = abs((query_end - query_start) - (ref_end_cand - ref_start_cand) - sv_len)
+                ref_start_best = ref_start_cand
+                ref_end_best = ref_end_cand
+                query_start_best = query_start
+                query_end_best = query_end
+                start_contig_name_ctr = start_contig_name_ctr_cand
+                end_contig_name_ctr = end_contig_name_ctr
+                
+                if neg_strand_tep:
+                    neg_strand = True
+                else:
+                    neg_strand = False
+    
+    if query_start_best == None or query_end_best == None:
+        print("Wrong query pos")
+        message = "Wrong_query_pos"
+        #write_err(output_file_name, message, g)
+        continue
+    
+    ref_start = ref_start_best
+    ref_end = ref_end_best
+    query_start = query_start_best
+    query_end = query_end_best
+    
+    if ref_start == sv_pos:
+        ref_start = ref_start - 1
+    
+    if ref_end == sv_end:
+        ref_end = ref_end + 1
+
+    if query_end == query_start:
+        query_end += 1
+        #continue
+    
+    query_name = contig_name_dict[start_contig_name_ctr]
+    query_rec = query_fasta_file.fetch(query_name)
+    
+    if query_start >= len(query_rec) or query_end >= len(query_rec):
+        message = "bad_query_pos"
+        #write_err(output_file_name, message, g)
+        continue
+    
+    #case 1: DEL
+    if sv_type == "DEL":
+        #query and ref seq fragment
+        query_frag = query_rec[query_start:query_end]
+        ref_frag = ref_rec[ref_start:ref_end]
+        #TODO: this is for DEL
+        #TODO: check +-1
+        ref_afterSV_frag1 = ref_rec[ref_start:sv_pos]
+        ref_afterSV_frag2 = ref_rec[sv_end:ref_end]
+
+        #alignment starts here
+        if neg_strand:
+            from Bio.Seq import Seq
+            seq = Seq(query_frag)
+            query_frag = seq.reverse_complement()
+            #query_frag = query_frag.reverse_complement()
+        
+        #get to upper case
+        ref_frag = ref_frag.upper()
+        ref_afterSV_frag1 = ref_afterSV_frag1.upper()
+        ref_afterSV_frag2 = ref_afterSV_frag2.upper()
+        query_frag = query_frag.upper()
+
+        #TODO: fragments too long will cause memory problem
+        memory_limit = 20000
+        if len(str(query_frag)) > memory_limit or len(str(ref_frag)) > memory_limit:
+            message = "memory_limit"
+            #write_err(output_file_name, message, g)
+            continue
+        if len(str(ref_afterSV_frag1)) + len(str(ref_afterSV_frag2)) > memory_limit:
+            message = "memory_limit"
+            #write_err(output_file_name, message, g)
+            continue
+
+        #TODO: find a appropriate alignment parameters
+        #paras: match, mismatch, open gap, extend gap
+        alignment_beforeSV = pairwise2.align.globalms(str(query_frag), str(ref_frag), 1, -1, -1, -0.5)
+        alignment_afterSV = pairwise2.align.globalms(str(query_frag), str(ref_afterSV_frag1) 
+                                + str(ref_afterSV_frag2), 1, -1, -1, -0.5)
+	    #get correct query info format
+    
+    #case 1: INS
+    elif sv_type == "INS":
+        #query and ref seq fragment
+        query_frag = query_rec[query_start:query_end]
+        ref_frag = ref_rec[ref_start:ref_end]
+        #TODO: this is for DEL
+        #TODO: check +-1
+        ref_afterSV_frag1 = ref_rec[ref_start:sv_pos]
+        ref_afterSV_frag2 = ref_rec[sv_end:ref_end]
+        ins_seq = sv_query_seq
+
+        #alignment starts here
+
+        if neg_strand:
+            from Bio.Seq import Seq
+            seq = Seq(query_frag)
+            query_frag = seq.reverse_complement()
+            #query_frag = query_frag.reverse_complement()
+            
+        #get to upper case
+        ref_frag = ref_frag.upper()
+        ref_afterSV_frag1 = ref_afterSV_frag1.upper()
+        ref_afterSV_frag2 = ref_afterSV_frag2.upper()
+        ins_seq = ins_seq.upper()
+        query_frag = query_frag.upper()
+
+        #TODO: fragments too long will cause memory problem
+        memory_limit = 20000
+        if len(str(query_frag)) > memory_limit or len(str(ref_frag)) > memory_limit:
+            message = "memory_limit"
+            #write_err(output_file_name, message, g)
+            continue
+        if len(str(ref_afterSV_frag1)) + len(str(ins_seq)) + len(str(ref_afterSV_frag2)) > memory_limit:
+            message = "memory_limit"
+            #write_err(output_file_name, message, g)
+            continue
+            
+        if len(str(query_frag)) == 0 or len(str(query_frag)) == 0 or len(str(ref_afterSV_frag1)) + len(str(ins_seq)) + len(str(ref_afterSV_frag2)) == 0:
+            message = "Wrong seq!!!"
+            #write_err(output_file_name, message, g)
+            continue
+
+        #TODO: find a appropriate alignment parameters
+        #paras: match, mismatch, open gap, extend gap
+        alignment_beforeSV = pairwise2.align.globalms(str(query_frag), str(ref_frag), 1, -1, -1, -0.5)
+        alignment_afterSV = pairwise2.align.globalms(str(query_frag), str(ref_afterSV_frag1) + str(ins_seq)
+                                + str(ref_afterSV_frag2), 1, -1, -1, -0.5)
+	    #get correct query info format
+    
+    #Need to store the following information in order:
+    # SV_index score_before score_after no_found SV_type hap:1 or 2
+    # (counter alignment_beforeSV[0][2] alignment_afterSV[0][2] len(query_start_dic) sv_type hap)
+    # contig_seg_len ref_seg_len SV_len
+    # (len(query_frag) len(ref_frag) sv_end-sv_pos+1)
+    # chr_name SV_start SV_end interval_start interval_end
+    # (ref_name sv_pos sv_end ref_start ref_end)
+    # error message
+    # (err_mes)
+    # if_score_inc in one of a haplotypes (current)
+    # 
+    # if_align_failed failure_reasons (in current haplotype)
+    # 
+    # genotype from vcf: 0/1 or 1/1 
+
+    g.write(str(counter) + "\t")
+    g.write(str(alignment_beforeSV[0][2]) + "\t")
+    #g.write(str(1) + "\t")
+    g.write(str(alignment_afterSV[0][2]) + "\t")
+    #g.write(str(1) + "\t")
+    #g.write(str(len(query_start_dic)) + "\t")
+    g.write(str(1) + "\t")
+    g.write(str(sv_type) + "\t")
+    g.write(str(hap) + "\t")
+    g.write(str(len(query_frag)) + "\t")
+    g.write(str(len(ref_frag)) + "\t")
+    g.write(str(sv_len) + "\t")
+    #g.write(str(sv_end-sv_pos+1) + "\t")
+    g.write(str(ref_name) + "\t")
+    g.write(str(sv_pos) + "\t")
+    g.write(str(sv_end) + "\t")
+    g.write(str(ref_start) + "\t")
+    g.write(str(ref_end) + "\t")
+    g.write(str(sv_type) + "\t")
+    g.write("noerr" + "\t")
+    g.write("\n")
+g.close()
